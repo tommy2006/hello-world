8c4d2329861b75930f82b1e0142daa5d6c95fae1
tommy2006 <45487455+tommy2006@users.noreply.github.com>
2018-12-02 10:27:47 +0700

    Ported to Python 3.6

--- math helper v1.1.py
+++ math helper v1.1.py
@@ -1,21 +1,21 @@
 print ("I can help you with some math. I am the IntelCalculator v1.1. I have been modified to run on Python 3.6.1.")
 print ("What do you want me to do?")
-g = raw_input()
+g = input()
 if g == ("Calculate the area of a shape."):
                  print ("Enter the shape you want to calculate") # Confirming which shape user wants to be calculated
-                 a = raw_input()
+                 a = input()
                  if a == ("triangle"):
                          print ("Enter the bottom length of the triangle")
-                         b = float(raw_input())
+                         b = float(input())
                          print ("Enter the height of the triangle")
-                         c = float(raw_input())
+                         c = float(input())
                          d = b*c/2
                          print ("The triangle's area is",x,"squared units.")
                  elif a == ("rectangle"):
                          print ("Enter the length of the rectangle")
-                         e = float(raw_input())
+                         e = float(input())
                          print ("Enter the width of the rectangle")
-                         f = float(raw_input())
+                         f = float(input())
                          class Rectangle():
                              def __init__(self,y,z):
                                  self.length = y
@@ -30,21 +30,21 @@ if g == ("Calculate the area of a shape."):
                          print ("The area of the rectangle is",a.getArea(),"squared units.")
                  elif a == ("square"):
                          print ("Enter the side length of the square")
-                         h = float(raw_input())
+                         h = float(input())
                          i = h*h
                          print ("The area of the square is",f,"squared units.")
 elif g == ("Calculate the percentage of one number to another."):
                  print ("Enter your first number")
-                 j = float(raw_input())
+                 j = float(input())
                  print ("Enter your second number")
-                 k = float(raw_input())
+                 k = float(input())
                  percentage = j/k*100
                  print ("The percentage of",h,"to",i,"is",percentage,"%.")
 elif g == ("Check divisibility of x to y"):
                  print ("Enter your first number.")
-                 l = float(raw_input())
+                 l = float(input())
                  print ("Enter your second number.")
-                 m = float(raw_input())
+                 m = float(input())
                  def checkdiv(x,y):
                                 if x>=y:
                                         if x%y == 0:
@@ -56,10 +56,10 @@ elif g == ("Check divisibility of x to y"):
                  checkdiv(l,m)
 elif g == ("Calculate perimeters of shapes"):
     print ("Which shape do you want to calculate?")
-    n = raw_input()
+    n = input()
     if n == ("square"):
         print ("Enter the side length of the square")
-        o = float(raw_input())
+        o = float(input())
         class Square():
             def perimeter(self,side):
                return side*4
@@ -67,9 +67,9 @@ elif g == ("Calculate perimeters of shapes"):
         print ("The square's perimeter is",p.perimeter(o),"units.")
     elif n == ("rectangle"):
         print ("Enter the length of the rectangle")
-        q = float(raw_input())
+        q = float(input())
         print ("Enter the breadth of the rectangle")
-        r = float(raw_input())
+        r = float(input())
         class Rectangle():
              def __init__(self,y,z):
                  self.length = y
@@ -80,22 +80,32 @@ elif g == ("Calculate perimeters of shapes"):
         print ("The perimeter of the rectangle is",a.getPerimeter(),"units.")
     elif n == ("pentagon"):
         print ("Enter the side length of the pentagon")
-        s = float(raw_input())
+        s = float(input())
         perimeter = s*5
         print ("The perimeter of the pentagon is",perimeter,"units.")
 elif g == ("Converting units"):
     print ("What type of measurenent units do you want to convert? (Volume, Weight, Length,...)")
-    measurementType = raw_input()#If I transfer this program to Python 3, change raw_input() into input()
+    measurementType = input()#If I transfer this program to Python 3, change raw_input() into input()
     if measurementType == ("Volume"):
         print ("Enter your first unit")
-        unit1 = raw_input()
+        unit1 = input()
         print ("How many units of that measurement unit do you want to convert?")
-        num1 = float(raw_input())
+        num1 = float(input())
         print ("Enter your second unit")
-        unit2 = raw_input()
+        unit2 = input()
         if unit1 == "ml" and unit2 == "cl":
             total = num1*0.1
-            print (num1,unit1,"(either singular or plural) equals",total,unit2,"(singular or plural again).")
+            print (num1,unit1,"equals",total,unit2)
+     elif measurementType == ("Weight"):
+        print ("Enter your first unit")
+        unit1 = input()
+        print ("How many units of",unit1,"do you want to convert?") 
+        num2 = float(input())
+        print ("Enter your second unit")
+        unit2 = input()
+        if unit1 == "kg" and unit2 == "lb":
+            total = num1*2.20462
+            print (num1,unit1,"equals",total,unit2)
                 
 
 
